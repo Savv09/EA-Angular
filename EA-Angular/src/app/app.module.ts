a00b1bfb5027750e4df7e7190e57a82bb7f7c9de
Gabriela Olteanu <gabrielaolteanu15@gmail.com>
2022-03-22 18:28:35 +0100

    Add blackbar

--- EA-Angular/src/app/app.module.ts
+++ EA-Angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { BlackbarComponent } from './blackbar/blackbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    BlackbarComponent
   ],
   imports: [
     BrowserModule
---
+++ EA-Angular/src/app/blackbar/blackbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blackbar',
+  templateUrl: './blackbar.component.html',
+  styleUrls: ['./blackbar.component.css']
+})
+export class BlackbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
