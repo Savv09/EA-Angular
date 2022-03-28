ab792bcdcbed04bbf8196b9aef24f42e89696404
axilo2 <caputom2@gmail.com>
2022-03-28 19:31:16 +0200

    add-tabs

--- EA-Angular/src/app/components/tabs/tabs.component.ts
+++ EA-Angular/src/app/components/tabs/tabs.component.ts
@@ -9,6 +9,12 @@ export class TabsComponent implements OnInit {
 
   constructor() { }
 
+  tabs: string[] = [
+    
+      'Notizie EA', 'EA Play', 'Madden NFL', 'Apex Legends', 'FIFA', 'Battlefield', 'The Sims 4', 'F1 2021',  'Inside EA'
+  
+  ];
+
   ngOnInit(): void {
   }
 
