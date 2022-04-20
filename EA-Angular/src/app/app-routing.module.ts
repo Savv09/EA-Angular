75d80551367ccf621be0f12280bee68d621e3d05
Leonardo Di Salvo <leonardodisalvo99@gmail.com>
2022-04-20 19:28:32 +0200

    fixed visual bug and routes

--- EA-Angular/src/app/app-routing.module.ts
+++ EA-Angular/src/app/app-routing.module.ts
@@ -1,27 +1,25 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { TemplateContainerComponent } from "./BaseTemplate/template-container/template-container.component";
-import { MainPageComponent } from "./main-page/main-page.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TemplateContainerComponent } from './BaseTemplate/template-container/template-container.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 const routes: Routes = [
-    {path: 'home', component: TemplateContainerComponent, 
-        children: [
-            {path: '', redirectTo: 'main', pathMatch: 'full'},
-            {path: '**', redirectTo: 'main', pathMatch: 'full' },
-            {path:'main', component: MainPageComponent},
-            
-        ]},
-    
-    {path: '', redirectTo: '/home', pathMatch: 'full' },
-    {path: '**', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'home',
+    component: TemplateContainerComponent,
+    children: [
+      { path: '', component: MainPageComponent, pathMatch: 'full' },
+      { path: '**', redirectTo: 'main', pathMatch: 'full' },
+      { path: 'main', component: MainPageComponent },
+    ],
+  },
 
-]
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],  
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule {}
