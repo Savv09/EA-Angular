d429a06e236f2f0c5b3798f30f87c5fcf80fd7e7
Leonardo Di Salvo <leonardodisalvo99@gmail.com>
2022-04-22 15:56:49 +0200

    solved visual media query bug

--- EA-Angular/src/app/components/buttons/primary-button/primary-button.component.ts
+++ EA-Angular/src/app/components/buttons/primary-button/primary-button.component.ts
@@ -3,13 +3,11 @@ import { IButtons } from 'src/models/buttons';
 @Component({
   selector: 'app-primary-button',
   templateUrl: './primary-button.component.html',
-  styleUrls: ['./primary-button.component.css']
+  styleUrls: ['./primary-button.component.css'],
 })
 export class PrimaryButtonComponent implements OnInit {
-  @Input()  buttonPrimary!: IButtons
-  constructor() { }
+  @Input() buttonPrimary!: IButtons;
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
