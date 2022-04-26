13034a7c113e3dcc74952a63b89db6cf209b1730
axilo2 <caputom2@gmail.com>
2022-04-26 18:00:49 +0200

    sign-in-page

--- EA-Angular/src/app/app-routing.module.ts
+++ EA-Angular/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TemplateContainerComponent } from './BaseTemplate/template-container/template-container.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 import { MainPageComponent } from './main-page/main-page.component';
 
 const routes: Routes = [
@@ -14,6 +16,8 @@ const routes: Routes = [
     ],
   },
 
+  { path: 'log-in', component: LogInComponent },
+  { path: 'sign-in', component: SignInComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home', pathMatch: 'full' },
 ];
